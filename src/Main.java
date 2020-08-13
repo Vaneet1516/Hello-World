4cab8f4e08691cf615921c53ba700f384986ab0d
saini <saini@10.0.0.164>
2020-08-13 16:50:34 -0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!"); 
+		System.out.println("My favorite video game is GTA 5."); 
+		System.out.println("I like swimming and biking."); 
+		System.out.println("Pizza is my favorite food."); 
+		System.out.println("I plan on going to a UC.");
+		System.out.println("I want to major in computer science."); 
+
+	}
+
+}
